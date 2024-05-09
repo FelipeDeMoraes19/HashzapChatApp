7526eb0aa71d0080b070dddcf055176cb06667a8
FelipeMoraes18 <111572342+FelipeMoraes18@users.noreply.github.com>
2024-05-09 20:09:30 -0300

    function responsible for receiving chat messages and displaying them in the graphical interface

--- codigo.py
+++ codigo.py
@@ -6,3 +6,19 @@ def main(pagina):
     chat = ft.Column()
 
     nome_usuario = ft.TextField(label="Escreva seu nome")
+
+    def enviar_mensagem_tunel(mensagem):
+        tipo = mensagem["tipo"]
+        if tipo == "mensagem":
+            texto_mensagem = mensagem["texto"]
+            usuario_mensagem = mensagem["usuario"]
+            chat.controls.append(ft.Text(f"{usuario_mensagem}: {texto_mensagem}"))
+        else:
+            usuario_mensagem = mensagem["usuario"]
+            chat.controls.append(ft.Text(f"{usuario_mensagem} entrou no chat", 
+                                         size=12, italic=True, color=ft.colors.ORANGE_500))
+        pagina.update()
+
+    pagina.pubsub.subscribe(enviar_mensagem_tunel)
+
+    
